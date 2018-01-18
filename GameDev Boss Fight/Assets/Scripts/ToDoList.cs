eaab3a4c96844266c0240fb4a5d3a493e6f58d3c
Unknown <ap682814@ohio.edu>
2018-01-18 12:05:13 -0500

    Austin ToDoList
    script that has most of austin's stuff

---
+++ GameDev Boss Fight/Assets/Scripts/ToDoList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToDoList : MonoBehaviour 
+{
+	//Character movement Script
+		//attack 
+		//move
+		//dodge
+		//take dmg
+		//victory?
+	//boss script
+		//under half health phase 2 (insert evil laugh)
+		//attack
+		//dodge?
+		//
+	//weapon script //can be one script then expanded into 3 for each weapon
+	//UI will need player health bar maybe stamina if we add in a stamina
+	//UI will have boss health bar
+
+
+}
