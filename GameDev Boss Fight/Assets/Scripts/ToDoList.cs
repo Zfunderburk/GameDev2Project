07fed20cae5115637908acf349f5de7fa472443c
Unknown <ap682814@ohio.edu>
2018-01-18 13:18:21 -0500

    To Do list update
    milestones added

--- GameDev Boss Fight/Assets/Scripts/ToDoList.cs
+++ GameDev Boss Fight/Assets/Scripts/ToDoList.cs
@@ -14,10 +14,39 @@ public class ToDoList : MonoBehaviour
 		//under half health phase 2 (insert evil laugh)
 		//attack
 		//dodge?
-		//
+		//weapon change?
 	//weapon script //can be one script then expanded into 3 for each weapon
 	//UI will need player health bar maybe stamina if we add in a stamina
 	//UI will have boss health bar
 
+
+
+
+	//Milestones
+	//1:Austin- Player controller on controller working/finished
+	//	Zach- Build arena in Unity (Probuilder?) both rooms Level design (placement of boss and items in level) (scaling)
+	//	Simon- Working on character (something human looking)
+
+	//2:Austin- Player controller polished have a start on boss AI
+	//	Zach-	UI (Player health stamina and boss health)
+	//	Simon- Base Model for the character done
+
+	//3:Austin- Moving Ai that can track and approach player (maybe attack?)
+	//	Zach-	Start implementing into Unity animations and models (Know how to implement animations and models so not learning on the spot)
+	//	Simon-	started animations for character (walk, idle)
+
+	//4:Austin- Ai can attack (at least one basic attack sequence where he approaches hits and leaves)
+	//	Zach-	Have animator set up (idle to walk walk to idle etc)
+	//	Simon-	Have walk idle run and attack swing done working on dodge (sidestep) for character
+
+	//5:Austin-	Polish boss Ai
+	//	Zach-	Sounds and secondary things helping out
+	//	Simon-	sidestep done have boss built from player model (scale up)
+
+	//6:Austin-	Bug Hunt and Polish
+	//	Zach- Bug Hunt and Polish
+	//	Simon- Have animations for boss done add extra if time or beg for help if behind
+
+	//7:Final game due
 
 }
