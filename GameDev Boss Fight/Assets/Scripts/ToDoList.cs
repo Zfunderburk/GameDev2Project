edd34ff805be30715a0f488aaf9feb95437cbca8
Unknown <ap682814@ohio.edu>
2018-01-28 22:53:10 -0500

    Started the tutorial
    doing a souls like tutorial because that way i have experience working with the animations and models

--- GameDev Boss Fight/Assets/Scripts/ToDoList.cs
+++ GameDev Boss Fight/Assets/Scripts/ToDoList.cs
@@ -8,10 +8,9 @@ public class ToDoList : MonoBehaviour
 
 	//Character movement Script
 		//attack 
-		//move
 		//dodge
+		//camera lock on
 		//take dmg
-		//victory?
 	//boss script
 		//under half health phase 2 (insert evil laugh)
 		//attack
@@ -21,7 +20,7 @@ public class ToDoList : MonoBehaviour
 	//UI will need player health bar maybe stamina if we add in a stamina
 	//UI will have boss health bar
 
-
+	//Make it so you can not enter arena without having a weapon
 
 
 	//Milestones
