c6482506b8fd17ca872bfdba3c016b7616317955
Unknown <ap682814@ohio.edu>
2018-02-01 13:16:54 -0500

    Part 2 Tutorial
    Handle Animations part

---
+++ GameDev Boss Fight/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AP
+{
+	
+
+public class InputHandler : MonoBehaviour
+{
+	float vertical, horizontal;
+
+	StateManager states;
+
+		CameraManager camManager;
+
+		float delta;
+
+	void Start () 
+	{
+			states = GetComponent<StateManager> ();
+			states.Init ();
+
+			camManager = CameraManager.singleton;
+			camManager.Init (this.transform);
+	}
+
+
+	void FixedUpdate()
+	{
+		delta = Time.fixedDeltaTime;
+		GetInput ();
+		UpdateStates ();
+		states.FixedTick (delta);
+	}
+
+	void Update()
+	{			
+		camManager.Tick (delta);
+	}
+
+	void GetInput()
+	{
+		vertical = Input.GetAxis ("Vertical");
+		horizontal = Input.GetAxis ("Horizontal");
+	}
+	// Update is called once per frame
+	void UpdateStates () 
+	{
+			states.horizontal = horizontal;
+			states.vertical = vertical;
+
+			Vector3 v = vertical * camManager.transform.forward;
+			Vector3 h = horizontal * camManager.transform.right;
+			states.moveDir = (v + h).normalized;
+			float m = Mathf.Abs (horizontal) + Mathf.Abs (vertical);
+			states.moveAmount = Mathf.Clamp01 (m);
+
+
+	}
+}
+}
---
+++ GameDev Boss Fight/Assets/Scripts/StateManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AP
+{
+	
+
+public class StateManager : MonoBehaviour 
+	{
+		[Header("INput")]
+		public float vertical, horizontal;
+		public float moveAmount;
+		public Vector3 moveDir;
+
+		[Header("Stats")]
+		public float moveSpeed = 2f;
+		public float runSpeed = 3.5f;
+
+		[Header("States")]
+		public bool run;
+
+		public GameObject activeModel;
+		public Animator anim;
+		public Rigidbody myBody;
+
+
+		public float delta;
+
+		public void Init()
+		{
+			SetUpAnimator ();
+			myBody = GetComponent<Rigidbody> ();
+			myBody.angularDrag = 999;
+			myBody.drag = 4;
+			myBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+		}
+
+		void SetUpAnimator()
+		{
+			if (activeModel == null)
+			{
+				anim = GetComponentInChildren<Animator> ();
+				if (anim == null)
+				{
+					Debug.Log ("You forgot a model.");
+				}
+				else
+				{
+					activeModel = anim.gameObject;
+				}
+			}
+			if (anim == null)
+			{
+				anim = activeModel.GetComponent<Animator> ();
+			}
+			anim.applyRootMotion = false;
+		}
+
+		public void FixedTick(float d)
+		{
+			delta = d;
+
+
+			//myBody.drag = (moveAmount > 0) ? 0 : 4; this is the same as the if else underneath
+			if (moveAmount > 0)
+			{
+				myBody.drag = 0;
+			}
+			else
+			{
+				myBody.drag = 4;
+			}
+
+			float targetSpeed = moveSpeed;
+			if (run)
+			{
+				targetSpeed = runSpeed;
+			}
+
+			myBody.velocity = moveDir * (targetSpeed * moveAmount);
+		}
+
+	}
+}
