f133003ea6ca00b445db0d1453596a655a76119b
Zach Funderburk <zf547414@ohio.edu>
2018-03-06 13:16:45 -0500

    Drop Weapon Start

--- GameDev Boss Fight/Assets/Scripts/_Scripts/WeaponParent.cs
+++ GameDev Boss Fight/Assets/Scripts/_Scripts/WeaponParent.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class WeaponParent : MonoBehaviour {
 
-//	public GameObject weapon1;
-//	public GameObject weapon2;
-//	public GameObject weapon3;
+	public GameObject handle;
 
-	public GameObject hand;
-
-//	public Transform handTrans;
-
-//	public Vector3 handPoint;
 
 
 	void OnTriggerEnter (Collider other)
 	{
-		transform.parent = hand.transform;									//Parents, but doesnt set to hand Position
+		transform.parent = handle.transform;								//Parents, but doesnt set to hand Position
 		transform.localPosition = new Vector3(0,0,0);						//Sets Weapon to the local position of Hand
 		transform.localRotation = Quaternion.identity;						//Sets Weapon to the local rotation of Hand
 
 	}
+
+	void DropWeapon ()
+	{
+		if (Input.GetKeyDown ("t"))
+		{
+			transform.parent = null;
+			Debug.Log ("Weapon Dropped");
+		}
+	}
+
+
 }
