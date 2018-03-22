b0299941e9bb1e1a68b17842922377a5703b17dc
Zach Funderburk <zf547414@ohio.edu>
2018-03-22 13:14:58 -0400

    Model, Sword Additions

---
+++ GameDev Boss Fight/Assets/Scripts/_Scripts/PlayerStamina.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStamina : MonoBehaviour 
+{
+//	public StateManager
+
+	public int starterStamina = 100;
+	public float currentStamina;
+	public Slider StaminaSlider;
+	public Image StaminaFill;
+
+	public bool damaged;
+
+	void Awake () 
+	{
+		currentStamina = starterStamina;
+	}
+
+
+	void Update () 
+	{
+
+	}
+
+}
