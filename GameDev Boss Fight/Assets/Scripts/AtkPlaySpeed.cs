ad20b7f6fbcaedfa66bb8cb92d371f05df1bf08f
Zach Funderburk <zf547414@ohio.edu>
2018-04-05 13:21:41 -0400

    Anim Speed

---
+++ GameDev Boss Fight/Assets/Scripts/AtkPlaySpeed.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtkPlaySpeed : MonoBehaviour 
+{
+	public Animator anim;
+	public Animation atk;
+
+	public float atkSpeed;
+
+	void Start () 
+	{
+		anim = GetComponent<Animation>();
+	}
+
+	void Update ()
+	{
+		atk["oh_attack_1"].speed = atkSpeed;
+	}
+
+
+}
