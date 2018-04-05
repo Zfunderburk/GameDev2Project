c1bbd4173e7baa545e96a3b62be253dd5479e1bd
Zach Funderburk <zf547414@ohio.edu>
2018-04-05 13:08:17 -0400

    Weapon Fix(?) and Dummy Add

---
+++ GameDev Boss Fight/Assets/Scripts/AnimEventReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimEventReceiver : MonoBehaviour 
+{
+	public AP.WeaponHook weapon1;
+	public AP.WeaponHook weapon2;
+	public AP.WeaponHook weapon3;
+
+	public void OpenColliders ()
+	{
+		weapon1.OpenDamageColliders ();
+		weapon2.OpenDamageColliders ();
+		weapon3.OpenDamageColliders ();
+	}
+
+
+	public void CloseColliders ()
+	{
+		weapon1.CloseDamageColliders ();
+		weapon2.CloseDamageColliders ();
+		weapon3.CloseDamageColliders ();
+	}
+}
--- GameDev Boss Fight/Assets/Scripts/WeaponHook.cs
+++ GameDev Boss Fight/Assets/Scripts/WeaponHook.cs
@@ -8,19 +8,30 @@ namespace AP
 	public class WeaponHook : MonoBehaviour 
 	{
 		public GameObject[] damageCollider;
+		public Collider [] damColliders;
 
 		public void OpenDamageColliders()
 		{
-			for (int i = 0; i < damageCollider.Length; i++)
-			{
-				damageCollider [i].SetActive (true);
+//			for (int i = 0; i < damageCollider.Length; i++)
+//			{
+//				damageCollider [i].SetActive (true);
+//			}
+
+			for (int i = 0; i < damColliders.Length; i++)
+			{				
+				damColliders [i].enabled = true;
 			}
 		}
 		public void CloseDamageColliders()
 		{
-			for (int i = 0; i < damageCollider.Length; i++)
-			{
-				damageCollider [i].SetActive (false);
+//			for (int i = 0; i < damageCollider.Length; i++)
+//			{
+//				damageCollider [i].SetActive (false);
+//			}
+
+			for (int i = 0; i < damColliders.Length; i++)
+			{				
+				damColliders [i].enabled = false;
 			}
 		}
 
