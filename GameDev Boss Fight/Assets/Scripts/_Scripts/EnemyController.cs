b2c27c0521625e4a7b66117656d5cc4d0e81a11c
Zach Funderburk <zf547414@ohio.edu>
2018-04-08 15:18:25 -0400

    MainScene Adjustments

--- GameDev Boss Fight/Assets/Scripts/_Scripts/EnemyController.cs
+++ GameDev Boss Fight/Assets/Scripts/_Scripts/EnemyController.cs
@@ -8,33 +8,19 @@ public class EnemyController : MonoBehaviour
 	//public Transform player;
 	public GameObject spawnPoint;
 
-
-
 	public Canvas enemyHealth;
 
-	public bool canMove;
-	private float range;
-	private float minDistance = 1f;
-	private float speed = 3f;
-
 	Vector3 Movement;
-
 
 	void Start ()
 	{
 		enemyHealth.enabled = false;
-		canMove = false;
 
 	}
 
 	void OnTriggerEnter (Collider other)
 	{
 		enemyHealth.enabled = true;
-		canMove = true;
 	}
 
-	void Update()
-	{
-		
-	}
 }
