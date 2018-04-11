ae27153ca8a985bae0b6cc0e95cf95d8c9caf76b
Unknown <ap682814@ohio.edu>
2018-04-10 23:39:05 -0400

    Tried to fix run in for dmg
    didnt work

--- GameDev Boss Fight/Assets/Scripts/HooksandHelpers/DamageCollider.cs
+++ GameDev Boss Fight/Assets/Scripts/HooksandHelpers/DamageCollider.cs
@@ -7,17 +7,29 @@ namespace AP
 
 	public class DamageCollider : MonoBehaviour 
 	{
+        Collider[] childrenColliders;
         public Collider swordColliderFromPlayer;
+        public GameObject handle;
+        //EnemyStates eStates;
 
+
+		
 		private void Update()
 		{
-           // swordColliderFromPlayer =the weapon that the player is holding collider ;
-		}
-		void OnTriggerEnter(Collider other)
-		{
-            Debug.Log(other);
-            EnemyStates eStates = other.transform.transform.GetComponentInParent<EnemyStates> ();
+            // swordColliderFromPlayer =the weapon that the player is holding collider ;
+            //swordColliderFromPlayer = handle.GetComponentInChildren<Collider>();
+            childrenColliders = handle.GetComponentsInChildren<Collider>();
+            swordColliderFromPlayer = childrenColliders[1];
+            Debug.Log(childrenColliders[1]);
+           // Debug.Log(swordColliderFromPlayer);
 
+		}
+
+        void OnTriggerEnter(Collider swordColliderFromPlayer)
+		{
+            
+            EnemyStates eStates = swordColliderFromPlayer.transform.GetComponentInParent<EnemyStates> (); // this line is the issue
+            Debug.Log(swordColliderFromPlayer);
 			if (eStates == null)
 			{
 				return;
